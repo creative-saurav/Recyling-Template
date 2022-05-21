6974f3e33f352474d790b3fc0a9276de07254cea
Subir Sarkar Saurav <subirsarkar90283@gmail.com>
2022-05-21 09:57:56 +0600

    Full File Project

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $(".bars-icon").click(function(){
+        $(".header-right").slideToggle(1000);
+        // $(".contact-btn").show(1000);
+    });
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+});
